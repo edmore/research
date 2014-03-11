1451c1e328f05241dd474b5833167f331cc33a8e
Edmore Moyo <etmoyo@gmail.com>
2014-03-11 12:20:58 +0200

    oops breaking stuff

--- research.go
+++ research.go
@@ -3,12 +3,13 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("_site")))
 	fmt.Println("listening...")
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 	if err != nil {
 		panic(err)
 	}
